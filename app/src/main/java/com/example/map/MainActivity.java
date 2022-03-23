90d4882b2f8ab463f2c7bc283cc31432ad9663de
ab1872 <ab1872@scarletmail.rutgers.edu>
2022-03-23 19:43:22 -0400

    Initial

--- app/src/main/java/com/example/map/MainActivity.java
+++ app/src/main/java/com/example/map/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.map;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        /*
+                android:layout_width="match_parent"
+        android:layout_height="0dp"
+        android:background="#FFF9C4"
+        app:layout_constraintBottom_toTopOf="@+id/navigation"
+        app:layout_constraintTop_toTopOf="parent"
+
+
+
+         */
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 }
+
