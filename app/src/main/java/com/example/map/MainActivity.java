fa291cf6e87c93db1e85ffd82fffb61ed73d03a1
ab1872 <ab1872@scarletmail.rutgers.edu>
2022-03-30 14:45:19 -0400

    Initial

--- app/src/main/java/com/example/map/MainActivity.java
+++ app/src/main/java/com/example/map/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.map;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,27 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView img = findViewById(R.id.map_view);
-        img.setImageResource(R.drawable.ic_hallview);
-        img.setContentDescription(getResources().getString(R.string.app_name));
 
-        MapPicture map = new MapPicture(5);
-        img.setImageDrawable(map);
-
-        Thread thread = new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                try  {
-                    OutdoorMapData mapdata = new OutdoorMapData();
-                    mapdata.GetData(51.249,7.148,51.251,7.152);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        thread.start();
 
     }
 }
